ebf8f7cfa7db1acf13f79a4fe2e26bcc28f45a94
roscas <sol.rosca@he-arc.ch>
2018-08-24 21:27:39 +0200

    staging setup

---
+++ settings/staging.py
@@ -0,0 +1,146 @@
+import os
+from decouple import config
+
+from .data import WEBSITE_DATA
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+APPS_DIR = os.path.join(BASE_DIR, 'apps')
+
+
+SECRET_KEY = config('SECRET_KEY')
+
+
+DEBUG = False
+ALLOWED_HOSTS = ['142.93.175.164']
+
+SHELL_PLUS_POST_IMPORTS = [
+    ('spa1_API.utils', 'Tools'),
+]
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'django_extensions',
+    'corsheaders',
+    'rest_framework',
+
+    'apps.gallery',
+    'apps.main',
+    'apps.articles',
+]
+
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# CORS
+# CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = False
+# CORS_ORIGIN_WHITELIST= (
+#     'http://localhost:8080',
+#     'http://localhost:8080',
+# )
+
+ROOT_URLCONF = 'spa1_API.urls'
+
+
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'spa1_API.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/2.0/topics/i18n/
+
+LANGUAGE_CODE = 'fr'
+
+TIME_ZONE = 'Europe/Zurich'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.0/howto/static-files/
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Email
+
+EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
+EMAIL_HOST = config('EMAIL_HOST', default='smtp.mailgun.org')
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='postmaster@mg.jrosk.pw')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+
+DEFAULT_FROM_EMAIL = f"{WEBSITE_DATA['NAME'].strip('.')} <noreply@jrosk.pw>"
+EMAIL_SUBJECT_PREFIX = f"{WEBSITE_DATA['PROJECT_NAME']}: "
+EMAIL_SEND_TO = ['jrosk.ad@gmail.com']
---
+++ spa1_API/_data.py
@@ -0,0 +1,36 @@
+WEBSITE_DATA = {
+    "ORDER_CONF_URL_TIMESTAMP" : 2,
+
+    "EVENTS_GALLERY_LIMIT"     : 16,
+    "CAROUSEL_GALLERY_LIMIT"   : 8,
+    "PARALLAX_GALLERY_LIMIT"   : 3,
+    "ARTICLES_GALLERY_LIMIT"   : 100,
+    "PROJECT_NAME"             : "PersonnalWebsiteForall",
+    "NAME"                     : "MySite.",
+    "NAME_ADD"                 : "Forall",
+    "DESCRIPTION"              : "Hello world!",
+    "VILLE"                    : "LeMonde",
+    "CODE_POSTAL"              : "2000",
+    "ADRESSE"                  : "awesome street",
+    "OUVERTURE"                : "2016",
+
+
+    "TELEPHONE"                : "032-725-08-58",
+    "EMAIL"                    : "jrosk.ad@gmail.com",
+    "FACEBOOK"                 : "https://www.facebook.com/mcflysdinerneuchatel",
+    "GOOGLE"                   : "https://www.google.ch/maps/place/McFly's+Diner+Neuch%C3%A2tel/@46.9922131,6.9248096,15z/data=!4m2!3m1!1s0x0:0x399ca68c2bcc87de?sa=X&ved=0ahUKEwiZ6cPbroPZAhWMOhQKHdR_Cz8Q_BIIggEwCg",
+    "TWITTER"                  : "https://twitter.com/",
+    "INSTAGRAM"                : "https://www.instagram.com/?hl=en",
+    "LINKEDIN"                 : "https://www.linkedin.com/",
+    "SNAPCHAT"                 : "https://www.snapchat.com/",
+    "TRIPADVISOR"              : "https://www.tripadvisor.com/Restaurant_Review-g188066-d12069550-Reviews-McFly_s_Diner-Neuchatel.html",
+    "LOGO_PATH"                : "",
+
+    # fromagerie reussilles
+
+    "GOOGLE_API": "AIzaSyAKrXyewXcDPgvG66yHBmm3vFZDoJDDS1M",
+    "GOOGLE_PLACE_ID": "ChIJPwMEJQwCjkcRkmHo22dFFpo",
+    "GOOGLE_PLACE": 'https://goo.gl/scVfzx',
+    "GOOGLE_REVIEW_LINK_ALL": "https://goo.gl/9XWfVB",
+    "GOOGLE_REVIEW_LINK_WRITE": "https://goo.gl/owSQ5r",
+}
