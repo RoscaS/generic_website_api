e613467ff0808872608bac12d1a63c8b6c27ad0d
roscas <sol.rosca@he-arc.ch>
2018-09-04 11:58:06 +0200

    staging

--- settings/staging.py
+++ settings/staging.py
@@ -1,5 +1,10 @@
-import os
+import json, os
 from decouple import config
+
+from six.moves.urllib import request
+from cryptography.x509 import load_pem_x509_certificate
+from cryptography.hazmat.backends import default_backend
+
 
 from .data import WEBSITE_DATA
 
@@ -32,6 +37,8 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
 
+    'rest_framework_jwt',
+
     'apps.gallery',
     'apps.main',
     'apps.articles',
@@ -48,17 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# CORS
-# CORS_ORIGIN_ALLOW_ALL = True
-CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST= (
-    '142.93.175.164',
-    'jrosk.ch',
-)
-
 ROOT_URLCONF = 'spa1_API.urls'
-
-
 
 TEMPLATES = [
     {
@@ -144,3 +141,67 @@ EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
 DEFAULT_FROM_EMAIL = f"{WEBSITE_DATA['NAME'].strip('.')} <noreply@jrosk.pw>"
 EMAIL_SUBJECT_PREFIX = f"{WEBSITE_DATA['PROJECT_NAME']}: "
 EMAIL_SEND_TO = ['jrosk.ad@gmail.com']
+
+
+
+
+
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST= (
+    '142.93.175.164',
+    'jrosk.ch',
+)
+
+
+CORS_ORIGIN_WHITELIST = (
+    'jrosk.ch',
+)
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        # 'rest_framework_auth0.authentication.Auth0JSONWebTokenAuthentication',
+        # 'rest_framework.permissions.IsAuthenticated',
+        # 'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',  # allows secondary way
+    ),
+}
+
+
+AUTH0_DOMAIN = 'jrosk.eu.auth0.com'
+API_IDENTIFIER = 'http://jrosk.ch:8000'
+PUBLIC_KEY = None
+JWT_ISSUER = None
+
+if AUTH0_DOMAIN:
+    jsonurl = request.urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
+    jwks = json.loads(jsonurl.read().decode('utf-8'))
+    cert = '-----BEGIN CERTIFICATE-----\n' + jwks['keys'][0]['x5c'][
+        0] + '\n-----END CERTIFICATE-----'
+    certificate = load_pem_x509_certificate(cert.encode('utf-8'),
+                                            default_backend())
+    PUBLIC_KEY = certificate.public_key()
+    JWT_ISSUER = 'https://' + AUTH0_DOMAIN + '/'
+
+
+def jwt_get_username_from_payload_handler(user):
+    email = user['http://jrosk.ch_EMAIL']
+    scope = user['scope']
+    if 'full_access' in user['scope']:
+        # print('\n=====has full_access=====\n')
+        return "admin"
+    # print('\n=====has NOT full_access=====\n')
+    # return False
+
+
+JWT_AUTH = {
+    'JWT_PAYLOAD_GET_USERNAME_HANDLER': jwt_get_username_from_payload_handler,
+    'JWT_PUBLIC_KEY': PUBLIC_KEY,
+    'JWT_ALGORITHM': 'RS256',
+    'JWT_AUDIENCE': API_IDENTIFIER,
+    'JWT_ISSUER': JWT_ISSUER,
+    'JWT_AUTH_HEADER_PREFIX': 'Bearer',
+}
