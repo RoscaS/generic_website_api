29df0131c9ab862a944eda4ec1a917cdadd47f56
roscas <sol.rosca@he-arc.ch>
2018-09-06 17:23:24 +0200

    maj dist

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -5,5 +5,12 @@ routeList = (
     (r'presentation', viewsets.PresentationViewSet),
     (r'hero', viewsets.HeroViewSet),
     (r'article', viewsets.ArticlesViewSet),
-
+    (r'events', viewsets.EventsViewSet),
+    (r'contact', viewsets.ContactViewSet),
+    (r'review', viewsets.ReviewViewSet),
+    (r'promo', viewsets.PromoViewSet),
+    (r'message', viewsets.MessageViewSet),
+    (r'siteInfo', viewsets.SiteInformationsViewSet),
+    (r'siteContact', viewsets.SiteContactViewSet),
+    (r'siteOptions', viewsets.SiteOptionsViewSet),
 )
