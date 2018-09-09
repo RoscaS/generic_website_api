0be9d81122a4191a47d58869b2c5bf020b70def3
roscas <sol.rosca@he-arc.ch>
2018-09-09 03:03:18 +0200

    maj dist

--- settings/staging.py
+++ settings/staging.py
@@ -16,7 +16,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 
 DEBUG = False
-ALLOWED_HOSTS = ['104.248.29.118', '.jrosk.ch']
+ALLOWED_HOSTS = ['jrosk.ch']
 
 SHELL_PLUS_POST_IMPORTS = [
     ('spa1_API.utils', 'Tools'),
@@ -148,11 +148,10 @@ EMAIL_SEND_TO = ['jrosk.ad@gmail.com']
 
 
 
-CORS_ORIGIN_ALLOW_ALL = True
-# CORS_ORIGIN_WHITELIST= (
-#     '104.248.29.118',
-#     'jrosk.ch',
-# )
+# CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST= (
+    'jrosk.ch',
+)
 
 
 REST_FRAMEWORK = {
