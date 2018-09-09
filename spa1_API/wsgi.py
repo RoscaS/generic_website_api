a2a68ff2c6bfb14ecc6b5e99c999bd409c84f997
roscas <sol.rosca@he-arc.ch>
2018-09-09 03:49:50 +0200

    test ssl

--- spa1_API/wsgi.py
+++ spa1_API/wsgi.py
@@ -11,8 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ['HTTPS'] = "on"
-os.environ['wsgi.url_scheme'] = "https"
+# os.environ['HTTPS'] = "on"
+# os.environ['wsgi.url_scheme'] = "https"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "spa1_API.settings")
 
 application = get_wsgi_application()
