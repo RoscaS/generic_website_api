ddecfa242c84ab98fdc4a47422511960e3ad153c
roscas <sol.rosca@he-arc.ch>
2018-09-11 03:24:08 +0200

    tests git-pull view

--- apps/tools/views.py
+++ apps/tools/views.py
@@ -8,8 +8,13 @@ from rest_framework.permissions import IsAuthenticated
 class GitPullView(APIView):
     # permission_classes = (IsAuthenticated,)
 
-    def get(self, request, fromat=None):
-        front = settings.FRONTEND_DIR
-        repo = git.Repo(front)
-        pull = repo.git.pull()
-        return Response(pull)
+    def get(self, request, format=None):
+        for k,v in request.META.items():
+            print(k+':',v)
+        print(request.META['HTTP_POULE'])
+        # front = settings.FRONTEND_DIR
+        # repo = git.Repo(front)
+        # pull = repo.git.pull()
+        # return Response(pull)
+        print('\n\n WORKING \n\n')
+        return Response('OK')
