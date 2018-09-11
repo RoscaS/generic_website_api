1db8d130a9f06348b2f274fcb1d43c54973375da
roscas <sol.rosca@he-arc.ch>
2018-09-11 20:42:51 +0200

    pre prod CJ

--- settings/staging.py
+++ settings/staging.py
@@ -179,7 +179,7 @@ if AUTH0_DOMAIN:
 
 
 def jwt_get_username_from_payload_handler(user):
-    email = user['https://jrosk.ch_EMAIL']
+    email = user['http://jrosk.ch:8000_EMAIL']
     if email == ADMIN_MAIL:
         return "admin"
 
